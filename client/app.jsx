8a8f6b2c89e7527050483a4ba3421141d9d77b44
Scarlet Kim <scarletkim714@gmail.com>
2022-09-17 05:54:46 +0000

    working on showing created memory

--- client/app.jsx
+++ client/app.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
 // import Home from './pages/home';
-// import Form from './pages/form';
+import Form from './pages/form';
 import Memories from './pages/memories';
-// import New from './pages/new-entries';
+import NewEntry from './pages/new-entry';
+import parseRoute from './lib/parse-route';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      route: parseRoute(window.location.hash)
+    };
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', event => {
+      this.setState({
+        route: parseRoute(window.location.hash)
+      });
+    });
+  }
+
+  renderPage() {
+    const { route } = this.state;
+    if (route.path === '') {
+      return <Form />;
+    }
+    if (route.path === 'new-entry') {
+      const entryId = route.params.get('entryId');
+      return <NewEntry entryId={entryId} />;
+    }
+    return null;
   }
 
   render() {
-    return <Memories />;
+    return (
+      <>
+      <Memories />
+      </>
+    );
     // : <Form />;
     // return <Home />;
   }
--- client/pages/new-entry.jsx
+++ client/pages/new-entry.jsx
@@ -3,12 +3,26 @@ import React from 'react';
 export default class NewEntry extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      entries: null
+    };
+  }
+
+  componentDidMount() {
+    fetch(`/api/memento/${this.entries.entryId}`)
+      .then(res => res.json())
+      .then(entry => this.setState({ entry }));
   }
 
   render() {
+    if (!this.state.entries) return null;
+    const { date, location, description } = this.state.entries;
     return (
-      <div>Hello</div>
+      <>
+      <div>${date}</div>
+      <div>${location}</div>
+      <div>${description}</div>
+      </>
     );
   }
 }
