8a2b3ff2a6289c9cf46e75e2dcb0c3fc50ad5be4
Scarlet Kim <scarletkim714@gmail.com>
2022-10-05 21:21:20 +0000

    removed unnecessary onClick method and added # for hashrouting to form. #9

--- client/components/nav.jsx
+++ client/components/nav.jsx
@@ -2,17 +2,13 @@ import React from 'react';
 
 export default class Nav extends React.Component {
 
-  onClick() {
-    window.location.hash = '#form';
-  }
-
   render() {
     return (
     <>
       <nav className="navbar navbar-dark bg-primary fixed-top">
         <div className="container px-3">
           <a className="navbar-brand" href="#">Memento</a>
-            <a className="btn btn-primary" href="form">
+            <a className="btn btn-primary" href="#form">
               <i className="fa fa-plus" />
             </a>
         </div>
